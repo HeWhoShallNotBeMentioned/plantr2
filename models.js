4d453ca817e5504cf299afa22b9d8249577de164
Chris Underwood <cunderwoodmn@gmail.com>
2018-11-12 16:56:12 -0600

    added seed files

--- models.js
+++ models.js
@@ -1,25 +1,26 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
-module.exports = db;
 
 const Gardener = db.define('gardeners', {
   name: Sequelize.STRING,
-  age: Sequelize.INTEGER
-})
+  age: Sequelize.INTEGER,
+});
 
 const Plot = db.define('plots', {
   size: Sequelize.INTEGER,
-  shaded: Sequelize.BOOLEAN
-})
+  shaded: Sequelize.BOOLEAN,
+});
 
 const Vegetable = db.define('vegetables', {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
-  planted_on: Sequelize.DATE
-})
+  planted_on: Sequelize.DATE,
+});
 
-Vegetable.belongsToMany(Plot, {through: 'vegetablesInPlot'});
-Plot.belongsToMany(Vegetable, {through: 'vegetablesInPlot'});
+Vegetable.belongsToMany(Plot, { through: 'vegetablesInPlot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetablesInPlot' });
 
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 Plot.belongsTo(Gardener);
+
+module.exports = { db, Vegetable, Plot, Gardener };
--- seed.js
+++ seed.js
@@ -1,13 +1,43 @@
-const db = require('./models');
+const { db, Vegetable, Plot, Gardener } = require('./models');
 
-db.sync({force: true})
+db.sync({ force: true })
   .then(() => {
     console.log('Database synced!');
+    let veg1 = Vegetable.create({
+      name: 'carrot',
+      color: 'orange',
+      planted_on: '2018-06-1',
+    });
+    let veg2 = Vegetable.create({
+      name: 'cabbage',
+      color: 'green',
+      planted_on: '2018-04-22',
+    });
+    let veg3 = Vegetable.create({
+      name: 'pumpkin',
+      color: 'black',
+      planted_on: '2018-10-31',
+    });
+    return Promise.all([veg1, veg2, veg3]);
   })
+  .then(() => {
+    return Gardener.create({
+      name: 'Jay',
+      age: 22,
+      favoriteVegetableId: 2,
+    }).then(gardener => {
+      console.log('gardner.....................', gardener.id);
+      return Plot.create({
+        size: 100,
+        shaded: 'yes',
+        gardenerId: gardener.id,
+      });
+    });
+  })
+
   .catch(error => {
     console.log('Something went wrong! Error: ', error);
   })
   .finally(() => {
     db.close();
-  })
-
+  });
